7fde3eeb1604dfb7c6e1df2f69938d990187e598
Tarun Chinmai <tchinmai7@gmail.com>
2017-01-29 21:25:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-sudo apt-get install -y dnsmasq lighthttpd
+sudo apt-get install -y python3 dnsmasq lighttpd
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 python3 fetchlists.py $ip
 rm -rf *.txt
 sort config_file.conf | uniq >/etc/dnsmasq.conf 
 rm -rf config_file.conf
 sudo mv adblock.conf /etc/dnsmasq.d/
-sudo mv /etc/lighthttpd.conf /etc/lighthttpd.conf.orig
-sudo cp lighthttpd.conf /etc/lighthttpd.conf
-sudo cp index.php /var/www/html/index.php
+sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.orig
+sudo cp lighttpd.conf /etc/lighttpd/lighttpd.conf
+sudo cp index.html /var/www/html/index.html
